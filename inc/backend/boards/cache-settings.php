6648d212a2f8b1f40bae12c7ab3f47b2330b7a21
Access Keys <Access Keys@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-04-29 10:30:25 +0000

    AccessPress Social Counter - Uodated some css for theme 4
    git-svn-id: https://plugins.svn.wordpress.org/accesspress-social-counter/tags/1.1.5@1148858 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ inc/backend/boards/cache-settings.php
@@ -0,0 +1,11 @@
+<div class="apsc-boards-tabs" id="apsc-board-cache-settings" style="display:none">
+	<div class="apsc-tab-wrapper">
+		<div class="apsc-option-inner-wrapper">
+			<label><?php _e('Cache Period','aps-counter');?></label>
+			<div class="apsc-option-field">
+				<input type="text" name="cache_period" value="<?php echo $apsc_settings['cache_period'];?>"/>
+				<div class="apsc-option-note"><?php _e('Please enter the time in hours in which the count should be updated.Default is 24 hours','aps-counter');?></div>
+			</div>
+		</div>
+	</div>
+</div>

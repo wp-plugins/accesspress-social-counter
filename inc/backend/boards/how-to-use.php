e27ae010f23d103cbe49344b443a4eb0e2957132
Access Keys <Access Keys@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-25 10:22:31 +0000

    AccessPress Social Counter -  Updated language file
    git-svn-id: https://plugins.svn.wordpress.org/accesspress-social-counter/tags/1.1.8@1166999 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ inc/backend/boards/how-to-use.php
@@ -0,0 +1,6 @@
+<div class="apsc-boards-tabs" id="apsc-board-how_to_use-settings" style="display:none">
+    <div class="apsc-tab-wrapper">
+        <p>To display the social profiles with counter, you can either use [aps-counter] <strong>Shortcode</strong> or you can use <strong>AccessPress Social Counter Widget</strong> from Appearance's widget section.</p>
+        <p>For the complete documentation please visit:<br/> <a href="https://accesspressthemes.com/documentation/documentation-plugin-instruction-accesspress-social-counter/" target="_blank">https://accesspressthemes.com/documentation/documentation-plugin-instruction-accesspress-social-counter/</a></p>
+    </div>
+</div>

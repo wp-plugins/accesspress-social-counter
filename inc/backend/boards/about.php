9642aa9703e1c520662a3726a2970e0bf9ec7973
Access Keys <Access Keys@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-07-01 07:03:24 +0000

    AccessPress Social Counter - Added a new shortcode to get the individual social media count
    git-svn-id: https://plugins.svn.wordpress.org/accesspress-social-counter/tags/1.3.0@1190615 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ inc/backend/boards/about.php
@@ -0,0 +1,63 @@
+<div class="apsc-boards-tabs" id="apsc-board-about-settings" style="display: none;">
+	<div class="apsc-tab-wrapper">
+		<p><strong>AccessPress Social Counter </strong> - is a FREE WordPress Plugin by AccessPress Themes. </p>
+
+		<p>AccessPress Themes is a venture of Access Keys - who has developed hundreds of Custom WordPress themes and plugins for its clients over the years. </p>
+
+		<p><strong>AccessPress Social Counter</strong> is a <strong>Free WordPress plugin</strong> to display your social accounts fans, subscribers and followers number on your website!
+A perfect plugin to show your social media stats and encourage more to join your network.
+
+You can select the social media profiles you want to activate, enter details of your social media and select one of the designs from beautifully designed 5 design themes. 
+
+All you have to do is either use a widget or shortcode to display your social media counter right on your website in your chosen location!
+
+The available social media in FREE version are: Facebook, Twitter, Google+, Instagram, Youtube, Sound Cloud and Dribbble along with Posts and Comments count. </p>
+		<div class="halfseperator"></div>
+		<p><strong>Please visit our product page for more details here:</strong><br />
+			<a href="https://accesspressthemes.com/wordpress-plugins/accesspress-social-counter/" target="_blank">https://accesspressthemes.com/wordpress-plugins/accesspress-social-counter/</a></p>
+			<div class="halfseperator"></div>
+			<p><strong>Plugin documentation can be found here:</strong><br />
+				<a href="https://accesspressthemes.com/documentation/documentation-plugin-instruction-accesspress-social-counter/" target="_blank">https://accesspressthemes.com/documentation/documentation-plugin-instruction-accesspress-social-counter/</a></p>
+				<div class="halfseperator"></div>
+				
+					
+						<h3>Other products by AccessPress themes </h3>
+						<div class="product">
+						<div class="logo-product"><img src="<?php echo SC_IMAGE_DIR;?>/aplite.png" alt="<?php esc_attr_e('AccessPress Social Counter','aps-counter'); ?>" /></div>
+						<div class="productext"><p><strong>AccessPress Lite</strong> - A very popular Free WordPress theme, available in WordPress.org<br />
+							<a href="http://accesspressthemes.com/wordpress-themes/accesspress-lite/" target="_blank">http://accesspressthemes.com/wordpress-themes/accesspress-lite/</a></p>
+						</div>
+						</div>
+
+						<div class="product">
+						<div class="logo-product"><img src="<?php echo SC_IMAGE_DIR;?>/appro.png" alt="<?php esc_attr_e('AccessPress Social Counter','aps-counter'); ?>" /></div>
+						<div class="productext"><p><strong>AccessPress Pro</strong> - Premium version of AccessPress lite<br />
+							<a href="http://accesspressthemes.com/wordpress-themes/accesspress-lite/" target="_blank">http://accesspressthemes.com/wordpress-themes/accesspress-pro/</a></p>
+						</div>
+						</div>
+
+						<div class="seperator"></div><div class="dottedline"></div><div class="seperator"></div>
+
+						<h3>Get in touch</h3>
+						<p>If you’ve any question/feedback, please get in touch: <br />
+							<strong>General enquiries:</strong> <a href="mailto:info@accesspressthemes.com">info@accesspressthemes.com</a><br />
+							<strong>Support:</strong> <a href="mailto:support@accesspressthemes.com">support@accesspressthemes.com</a><br />
+							<strong>Sales:</strong> <a href="mailto:sales@accesspressthemes.com">sales@accesspressthemes.com</a>
+						</p>
+						<div class="seperator"></div><div class="dottedline"></div><div class="seperator"></div>
+						<h3>Get social</h3>
+						<p>Get connected with us on social media. Facebook is the best place to find updates on our themes/plugins: </p>
+
+
+						<p><strong>Like us on facebook:</strong><br />
+							<iframe style="border: none; overflow: hidden; width: 764px; height: 206px;" src="//www.facebook.com/plugins/likebox.php?href=https%3A%2F%2Fwww.facebook.com%2Fpages%2FAccessPress-Themes%2F1396595907277967&amp;width=842&amp;height=258&amp;colorscheme=light&amp;show_faces=true&amp;header=false&amp;stream=false&amp;show_border=true&amp;appId=1411139805828592" width="240" height="150" frameborder="0" scrolling="no"></iframe></p>
+
+							<ul class="social-icon">
+								<li><a href="https://plus.google.com/u/0/+Accesspressthemesprofile/about" target="_blank"><img src="<?php echo SC_IMAGE_DIR;?>/googleplus.png" alt="google+"></a></li>
+								<li><a href="http://www.pinterest.com/accesspresswp/" target="_blank"><img src="<?php echo SC_IMAGE_DIR;?>/pinterest.png" alt="pinterest"></a></li>
+								<li><a href="https://www.flickr.com/photos/accesspressthemes/" target="_blank"><img src="<?php echo SC_IMAGE_DIR;?>/flicker.png" alt="flicker"></a></li>
+								<li><a href="https://twitter.com/apthemes" target="_blank"><img src="<?php echo SC_IMAGE_DIR;?>/twitter.png" alt="twitter"/></a></li>
+							</ul>
+
+						</div>
+					</div>

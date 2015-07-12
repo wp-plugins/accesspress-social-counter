e82280fa5f075fd66d99ee38e8a926ef76e7cd78
Access Keys <Access Keys@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-07-12 04:52:04 +0000

    AccessPress Social Counter - Added Facebook Default Count field
    git-svn-id: https://plugins.svn.wordpress.org/accesspress-social-counter/tags/1.3.1@1197275 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ inc/backend/boards/how-to-use.php
@@ -0,0 +1,9 @@
+<div class="apsc-boards-tabs" id="apsc-board-how_to_use-settings" style="display:none">
+    <div class="apsc-tab-wrapper">
+        <p>To display the social profiles with counter, you can either use [aps-counter] <strong>Shortcode</strong> or you can use <strong>AccessPress Social Counter Widget</strong> from Appearance's widget section.</p>
+        <p>For the complete documentation please visit:<br/> <a href="https://accesspressthemes.com/documentation/documentation-plugin-instruction-accesspress-social-counter/" target="_blank">https://accesspressthemes.com/documentation/documentation-plugin-instruction-accesspress-social-counter/</a></p>
+        <p>To get the individual count, please use below shortcode</p>
+        [aps-get-count social_media="facebook/twitter/googlePlus/instagram/youtube/soundcloud/dribbble/posts/comments"]
+        
+    </div>
+</div>
